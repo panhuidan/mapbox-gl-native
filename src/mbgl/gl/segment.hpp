f2f552bcfa2d5a03ebf05d34dda9e040970ee172
John Firebaugh <john.firebaugh@gmail.com>
2016-11-14 14:42:11 -0800

    [core] gl::Attribute support for data-driven properties

--- src/mbgl/gl/segment.hpp
+++ src/mbgl/gl/segment.hpp
@@ -1,12 +1,16 @@
 #pragma once
 
+#include <mbgl/gl/context.hpp>
+#include <mbgl/gl/vertex_buffer.hpp>
 #include <mbgl/util/optional.hpp>
 
 #include <cstddef>
+#include <vector>
 
 namespace mbgl {
 namespace gl {
 
+template <class Attributes>
 class Segment {
 public:
     Segment(std::size_t vertexOffset_,
@@ -24,13 +28,39 @@ public:
     std::size_t vertexLength;
     std::size_t indexLength;
 
+    void bind(Context& context,
+              BufferID indexBuffer_,
+              const typename Attributes::Locations& attributeLocations,
+              const typename Attributes::Values& attributeValues_) const {
+        if (!vao) {
+            vao = context.createVertexArray();
+        }
+
+        context.vertexArrayObject = *vao;
+
+        if (indexBuffer != indexBuffer_) {
+            indexBuffer = indexBuffer_;
+            context.elementBuffer.setDirty();
+            context.elementBuffer = indexBuffer_;
+        }
+
+        if (attributeValues != attributeValues_) {
+            attributeValues = attributeValues_;
+            Attributes::bind(context,
+                             attributeLocations,
+                             attributeValues_,
+                             vertexOffset);
+        }
+    }
+
 private:
-    friend class Context;
     mutable optional<UniqueVertexArray> vao;
+    mutable optional<BufferID> indexBuffer;
+    mutable optional<typename Attributes::Values> attributeValues;
 };
 
 template <class Attributes>
-class SegmentVector : public std::vector<Segment> {
+class SegmentVector : public std::vector<Segment<Attributes>> {
 public:
     SegmentVector() = default;
 };
